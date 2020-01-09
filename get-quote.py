f4837890eb36f18e984db66728891888f181a242
Eugene Sevostyanov <sevgenn@gmail.com>
2020-01-10 00:17:40 +0300

    Last changes of quotes

--- get-quote.py
+++ get-quote.py
@@ -8,8 +8,10 @@ def primary():
     f.close()
 
     last = len(quotes) - 1
-    rnd = random.randint(0, last)
-    print(quotes[rnd])
+    rnd1 = random.randint(0, last)
+    rnd2 = random.randint(0, last)
+    print(quotes[rnd1].rstrip())
+    print(quotes[rnd2].rstrip())
 
 if __name__== "__main__":
     primary()
